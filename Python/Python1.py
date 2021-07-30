ee1986f6160333868e8cd27636b33f1446335a95
Ryan-Shaffer <49421172+Ryan-Shaffer@users.noreply.github.com>
2021-07-30 18:08:23 -0400

    hapter 5 addition

--- Python/Python1.py
+++ Python/Python1.py
@@ -1,4 +1,39 @@
-inpt = (input('What planet do we live on?'))
-print (inpt,"is our planet",1,"\b:")
-#inpt2 = (input("What number planet is it?"))
+'''
+Input1 = (int(input('Enter the first #')))
+#print (inpt,"is our planet",1,"\b:")
+Input2 = (int(input("Enter the second #")))
 #print (inpt2)
+Sum = 0
+def Addition(Num1, Num2):
+    Sum = Num1 + Num2
+    return Sum
+    #print(Sum)
+Value = Addition(Input1,Input2)
+print(Addition)
+'''
+
+
+
+
+class Family:
+    '''Describes units of family'''
+    def __init__(self, role, sex, age):
+        '''Init Values'''
+        self.role = role
+        self.sex = sex
+        self.age = age
+    
+
+    def rolecall(self):
+        '''Returns information about a family member'''
+        role = self.role,self.sex,self.age
+        return role
+
+kelsey = Family('Wife','Female',"31")
+willy = Family('Pet','Male','4')
+ruby = Family('Baby','Female','10 Weeks')
+
+print(kelsey,'is my ',kelsey.rolecall())
+print('willy', 'is my', willy.rolecall())
+
+
